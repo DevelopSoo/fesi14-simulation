69b4674c89418cd5d15efbe79dd139b6bfa4d307
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-05-03 11:28:42 +0900

    버셀 배포

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // src/app/page.tsx
 
 export default function Home() {
-  return <h1>환경: {process.env.NEXT_PUBLIC_MY_ENVIRONMENT}</h1>;
+  return <h1>환경: {process.env.NEXT_PUBLIC_MY_ENVIRONMENT} vercel 배포</h1>;
 }
